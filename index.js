51c2349e39699ab6d084a1b50ee18ec81a2f80c1
sadekinchowdhury <rumelchow340@gmail.com>
2022-12-30 23:25:18 +0600

    ama

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.use(express.json())
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+// const { query } = require('express')
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oq7uvoj.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri)
@@ -18,18 +19,34 @@ function run() {
     try {
         const UserCollection = client.db('Inchat').collection('user')
         const postCollection = client.db('Inchat').collection('post')
-        const likeCollection = client.db('Inchat').collection('like')
+        const commentCollection = client.db('Inchat').collection('comment')
 
         app.post('/post', async (req, res) => {
             const post = req.body;
             const result = await postCollection.insertOne(post)
             res.send(result)
         })
-        // app.post('/like', async (req, res) => {
-        //     const post = req.body;
-        //     const result = await likeCollection.insertOne(post)
-        //     res.send(result)
-        // })
+        app.post('/comment', async (req, res) => {
+            const post = req.body;
+            const result = await commentCollection.insertOne(post)
+            res.send(result)
+        })
+        app.post('/users', async (req, res) => {
+            const post = req.body;
+            const result = await UserCollection.insertOne(post)
+            res.send(result)
+        })
+
+        app.get('/comment/:id', async (req, res) => {
+            const comment_id = req.params.id;
+            const query = { _id: ObjectId(comment_id) }
+            const result = await commentCollection.findOne
+
+                (query)
+            res.send(result)
+
+
+        })
         app.put('/post/:id', async (req, res) => {
             const post = req.body;
             const id = req.params.id;
@@ -61,9 +78,10 @@ function run() {
         })
 
 
+
         app.get('/allpost', async (req, res) => {
             const query = {}
-            const result = await postCollection.find(query).sort({ liking: -1 }).limit(3).toArray();
+            const result = await postCollection.find(query).sort({ liking: -1 }).limit(100).toArray();
             res.send(result)
         })
 
